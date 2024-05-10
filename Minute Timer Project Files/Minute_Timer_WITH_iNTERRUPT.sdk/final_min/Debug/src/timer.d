bb104afae0e4ef5c9d30a9bc78e0a7fab801cbfd
OscarRefachero <77154371+OscarRefachero@users.noreply.github.com>
2024-05-10 14:14:24 -0400

    All project files

---
+++ Minute Timer Project Files/Minute_Timer_WITH_iNTERRUPT.sdk/final_min/Debug/src/timer.d
@@ -0,0 +1,61 @@
+src/timer.o src/timer.o: ../src/timer.c \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../final_min_bsp/ps7_cortexa9_0/include/led_ip.h
+
+../../final_min_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../final_min_bsp/ps7_cortexa9_0/include/led_ip.h:
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Minute-Timer-for-Zybo-Z7-Board
 Simple Minute Timer with Count Up and Count Down Feature
+
+# HOW TO USE #
+- Open the project on vivado.
+- Generate bitstream if not already generated
+- Export hardware and launch SDK
+- Close any projects not associated with this one
+- Connect board to appropriate COM connection
+- Program FPGA
+- Right-click on the minute_timer folder, hover over Run As, and select Run on Hardware (GDB)
+- Enjoy the timer :)
